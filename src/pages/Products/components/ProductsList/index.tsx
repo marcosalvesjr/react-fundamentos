2b04c8d0d4854cafee2b4e630ac0dc0c4fd2eecb
Marcos Alves <mrcaurelio95@gmail.com>
2025-11-28 17:30:51 -0300

    add filters in ProductsList

--- src/pages/Products/components/ProductsList/index.tsx
+++ src/pages/Products/components/ProductsList/index.tsx
@@ -4,26 +4,84 @@ import {
   type OfficeProduct,
 } from "../../utils/productsList";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@radix-ui/react-select";
+import { Input } from "@/components/ui/input";
 
 export default function ProductsList() {
-  return (
-    <ul className="flex flex-col gap-5 w-full">
-      {products.map((item: OfficeProduct) => (
-        <li className="h-auto w-[90%] flex items-center justify-between px-2 py-1 border border-zinc-500 rounded-md ">
-          <div className="flex flex-col items-center">
-            <span>Produto: {item.name}</span>
-          </div>
-          <div className="flex gap-4">
-            <Button variant={"default"}>
-              <Trash width={22} height={22} color="red" />
-            </Button>
+  const [search, setSearch] = useState("");
+  const [filterType, setFilterType] = useState("");
+  const [filteredList, setFilteredList] = useState<OfficeProduct[]>([]);
 
-            <Button>
-              <Pencil width={22} height={22} color="green" />
-            </Button>
-          </div>
-        </li>
-      ))}
-    </ul>
+  function onCleanFilters() {
+    setSearch("");
+    setFilterType("");
+  }
+
+  useEffect(() => {
+    let result = products;
+    if (search.trim() !== "") {
+      result = result.filter((r) =>
+        r.name.toLowerCase().includes(search.toLowerCase().trim())
+      );
+    }
+
+    if (filterType !== "") {
+      result = result.filter((r) => r.type === filterType);
+    }
+    setFilteredList(result);
+  }, [search, filterType]);
+
+  // "material" | "eletronico" | "mobília" | "papelaria"
+  return (
+    <div className="flex flex-col gap-5">
+      <Input
+        value={search}
+        placeholder="Digite o nome do produto"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Select onValueChange={setFilterType}>
+        <SelectTrigger className="w-[280px] border border-white rounded-md p-2">
+          <SelectValue placeholder="Selecione o tipo do produto" />
+        </SelectTrigger>
+        <SelectContent className="w-[280px] bg-zinc-900 rounded-md p-2">
+          <SelectGroup>
+            <SelectLabel>Tipos</SelectLabel>
+            <SelectItem value="material">Material</SelectItem>
+            <SelectItem value="eletronico">Eletronico</SelectItem>
+            <SelectItem value="mobília">mobília</SelectItem>
+            <SelectItem value="papelaria">Papelaria</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Button onClick={() => onCleanFilters()}>Limpar filtros</Button>
+
+      <ul className="flex flex-col gap-5 w-full">
+        {filteredList.map((item: OfficeProduct) => (
+          <li className="h-auto w-[90%] flex items-center justify-between px-2 py-1 border border-zinc-500 rounded-md ">
+            <div className="flex flex-col items-center">
+              <span>Produto: {item.name}</span>
+            </div>
+            <div className="flex gap-4">
+              <Button variant={"default"}>
+                <Trash width={22} height={22} color="red" />
+              </Button>
+
+              <Button>
+                <Pencil width={22} height={22} color="green" />
+              </Button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ProductsList from "./components/ProductsList";
 
+
 export function Products() {
   return (
     <div>
       <h1>Produtos</h1>
+      <div>
+        <div>
+        </div>
+      </div>
+
       <ProductsList />
     </div>
   );
